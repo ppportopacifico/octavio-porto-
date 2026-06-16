147c396600ef7d737ac88f222232223b0afe1aad
carlos.octavio <carlos.octavio@aluno.edu.pi.gov.br>
2026-06-16 17:59:52 +0000

    aula ds

---
+++ objeto.js
@@ -0,0 +1,10 @@
+ const pessoa =
+ 
+    {
+    nome: "octavio porto",
+    email: "octavioporto@",
+    idade: 18,
+    ativo: true,
+    criadoEm: "2026-06-16T12:00:00Z"
+}
+console.log(pessoa.nome)
